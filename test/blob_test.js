06c7b5ddc2732923405cb4ee00e1856477d06213
elenasolomon <elenasolomon28@gmail.com>
2016-12-18 16:59:39 +0200

    After code review
    - now, blob tests are using 'suite' and 'test' instead of 'describe' and 'if'
    - in unit tests, the container name is no longer a random name.
    - constants.js was removed and, hardcoded values are used for HTTP verbs and HTTP response codes

--- test/blob_test.js
+++ test/blob_test.js
@@ -1,67 +1,67 @@
-var azure   = require('../');
-var assert  = require('assert');
-var uuid    = require('uuid');
+suite.only("Azure Blob", function() {
+  var azure   = require('../');
+  var assert  = require('assert');
 
-// Create azure blob client
-var blob = new azure.Blob({
-  accountId: process.env.AZURE_STORAGE_ACCOUNT,
-  accessKey: process.env.AZURE_STORAGE_ACCESS_KEY
-});
-
-describe.only('Azure Blob Container', function() {
-
-  it('create container with metadata', function() {
-    var containerName = uuid.v4();
-    var metadata = {
-      testKey: 'testValue'
-    };
-
-    return blob.createContainer(containerName, metadata, null)
-      .then(function(){
-        return blob.deleteContainer(containerName);
-      });
+  // Create azure blob client
+  var blob = new azure.Blob({
+    accountId: process.env.AZURE_STORAGE_ACCOUNT,
+    accessKey: process.env.AZURE_STORAGE_ACCESS_KEY
   });
 
-  it('create container without metadata', function() {
-    var containerName = uuid.v4();
+  suite("Container", function() {
+    test('create container with metadata', function() {
+      var containerName = 'fast-azure-blob-container-with-metadata';
+      var metadata = {
+        testKey: 'testValue'
+      };
 
-    return blob.createContainer(containerName, {}, null)
-      .then(function(){
-        return blob.deleteContainer(containerName);
-      });
-  });
+      return blob.createContainer(containerName, metadata, null)
+        .then(function(){
+          return blob.deleteContainer(containerName);
+        });
+    });
 
-  it('create container with container access', function() {
-    var containerName = uuid.v4();
+    test('create container without metadata', function() {
+      var containerName = 'fast-azure-blob-container';
 
-    // TODO verify the listing!!
-    return blob.createContainer(containerName, {}, null)
-      .then(function(){
-        return blob.deleteContainer(containerName);
-      });
-  });
+      return blob.createContainer(containerName, {}, null)
+        .then(function(){
+          return blob.deleteContainer(containerName);
+        });
+    });
 
-  it('set and get container metadata', function() {
-    var containerName = uuid.v4();
+    test('create container with container access', function() {
+      var containerName = 'fast-azure-blob-container-with-access';
 
-    // create a container without metadata
-    return blob.createContainer(containerName, {}, null)
-      .then(function(){
-        // set metadata to newly created container
-        var metadata = {
-          appName: 'fast-azure-storage'
-        }
-        return blob.setContainerMetadata(containerName, metadata);
-      })
-      .then(function(){
-        return blob.getContainerMetadata(containerName);
-      })
-      .then(function(result){
-        // verify if the metadata was correctly set
-        assert(result.appName === 'fast-azure-storage');
+      // TODO verify the listing!!
+      return blob.createContainer(containerName, {}, null)
+        .then(function(){
+          return blob.deleteContainer(containerName);
+        });
+    });
 
-        // delete the container
-        return blob.deleteContainer(containerName);
-      });
+    test('set and get container metadata', function() {
+      var containerName = 'fast-azure-blob-container-set-get-metadata';
+
+      // create a container without metadata
+      return blob.createContainer(containerName, {}, null)
+        .then(function(){
+          // set metadata to newly created container
+          var metadata = {
+            appName: 'fast-azure-storage'
+          }
+          return blob.setContainerMetadata(containerName, metadata);
+        })
+        .then(function(){
+          return blob.getContainerMetadata(containerName);
+        })
+        .then(function(result){
+          // verify if the metadata was correctly set
+          assert(result.appName === 'fast-azure-storage');
+
+          // delete the container
+          return blob.deleteContainer(containerName);
+        });
+    });
   });
 });
